58c65706244bb8ce9181ad3d63159f18c9a18a56
jmoney2998 <33417624+jmoney2998@users.noreply.github.com>
2017-12-12 11:44:23 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -70,7 +70,7 @@
 			# Decodifico la stringa json e la salvo nella variabile $data
 			$data = json_decode($json);
 			# Stampa della tabella delle pizzerie.
-			echo "<table class='table'>";
+			echo "<table>";
 				echo "<tr>";
 					echo "<th>NOME</th>";
 					echo "<th>LATITUDINE</th>";
